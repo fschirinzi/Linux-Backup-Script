5a2fad279a0e0bccdcd7a4a8719f0c395c89a0a2
fschirinzi <fschirinzi25@gmail.com>
2015-01-28 16:18:57 +0100

    Update sendinfos.sh

--- sendinfos.sh
+++ sendinfos.sh
@@ -6,26 +6,26 @@ body=$3
 file=$4
 
 
-sudo echo "$body" > info.txt
-sudo echo " " >> info.txt
-sudo echo " " >> info.txt
-sudo echo " " >> info.txt
-sudo echo "Linux-backup-lts Infos:" >> info.txt
-sudo echo "--------------------------------------------------------------------" >> info.txt
-sudo echo " " >> info.txt
-sudo df -kh >> info.txt
-sudo echo " " >> info.txt
-sudo echo "--------------------------------------------------------------------" >> info.txt
-sudo echo " " >> info.txt
-sudo free -m >> info.txt
-sudo echo " " >> info.txt
-sudo echo "--------------------------------------------------------------------" >> info.txt
-sudo echo " " >> info.txt
-sudo w >> info.txt
-sudo echo " " >> info.txt
-sudo echo "--------------------------------------------------------------------" >> info.txt
-sudo echo " " >> info.txt
-sudo who >> info.txt
+echo "$body" > info.txt
+echo " " >> info.txt
+echo " " >> info.txt
+echo " " >> info.txt
+echo "Linux-backup-lts Infos:" >> info.txt
+echo "--------------------------------------------------------------------" >> info.txt
+echo " " >> info.txt
+df -kh >> info.txt
+echo " " >> info.txt
+echo "--------------------------------------------------------------------" >> info.txt
+echo " " >> info.txt
+free -m >> info.txt
+echo " " >> info.txt
+echo "--------------------------------------------------------------------" >> info.txt
+echo " " >> info.txt
+w >> info.txt
+echo " " >> info.txt
+echo "--------------------------------------------------------------------" >> info.txt
+echo " " >> info.txt
+who >> info.txt
 
 if [ -z "$file" ]; then
         cat info.txt | mail -s "$subject" "$email"
@@ -33,4 +33,4 @@ else
         cat info.txt | mail -s "$subject" -a "$file" "$email"
 fi
 
-sudo rm info.txt
+rm -f info.txt
